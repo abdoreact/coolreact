7ff89d256dc4c6096828f29508900f8d934fd9ba
abdoabdoabdoabdoabdo <abdonaggar77@gmail.com>
2020-08-10 13:54:18 +0200

    Updates

---
+++ precache-manifest.b6c12a9f6a670b23de5b5f3a69e64a45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2478f44c95f3e9cfa4e6805e8f9a6220",
+    "url": "/github.io/coolreact/index.html"
+  },
+  {
+    "revision": "f85860486c347c63c977",
+    "url": "/github.io/coolreact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2add28918241bc4401d6",
+    "url": "/github.io/coolreact/static/js/2.4deb58ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/coolreact/static/js/2.4deb58ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f85860486c347c63c977",
+    "url": "/github.io/coolreact/static/js/main.775a0880.chunk.js"
+  },
+  {
+    "revision": "e2c7c05b79de07a6de51",
+    "url": "/github.io/coolreact/static/js/runtime-main.a3f75ed3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/coolreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
